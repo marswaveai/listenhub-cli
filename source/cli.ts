5ccc0fee6b8ca61b83bfa358df7611e6061373bc
0XFANGO <silencerichard@163.com>
2026-04-04 20:49:26 +0800

    feat: add podcast create/list commands

--- source/cli.ts
+++ source/cli.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import {Command} from 'commander';
 import {register as registerAuth} from './auth/_cli.js';
+import {register as registerPodcast} from './podcast/_cli.js';
 import {register as registerSpeakers} from './speakers/_cli.js';
 
 const program = new Command();
 program.name('listenhub').description('ListenHub CLI').version('0.1.0');
 
 registerAuth(program);
+registerPodcast(program);
 registerSpeakers(program);
 
 program.parse();
