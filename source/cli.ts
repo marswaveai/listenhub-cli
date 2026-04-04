1f310b7b6113add4edf0fc37f856cd6f4a5b85a3
0XFANGO <silencerichard@163.com>
2026-04-04 20:45:53 +0800

    feat: add speakers list command

--- source/cli.ts
+++ source/cli.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import {Command} from 'commander';
 import {register as registerAuth} from './auth/_cli.js';
+import {register as registerSpeakers} from './speakers/_cli.js';
 
 const program = new Command();
 program.name('listenhub').description('ListenHub CLI').version('0.1.0');
 
 registerAuth(program);
+registerSpeakers(program);
 
 program.parse();
