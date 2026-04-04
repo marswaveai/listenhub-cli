1548d89d85a1c63aa95a3ae7322cb3e3e2309617
0XFANGO <silencerichard@163.com>
2026-04-04 20:51:32 +0800

    feat: add tts create/list commands

--- source/cli.ts
+++ source/cli.ts
@@ -3,12 +3,14 @@ import {Command} from 'commander';
 import {register as registerAuth} from './auth/_cli.js';
 import {register as registerPodcast} from './podcast/_cli.js';
 import {register as registerSpeakers} from './speakers/_cli.js';
+import {register as registerTts} from './tts/_cli.js';
 
 const program = new Command();
 program.name('listenhub').description('ListenHub CLI').version('0.1.0');
 
 registerAuth(program);
 registerPodcast(program);
+registerTts(program);
 registerSpeakers(program);
 
 program.parse();
